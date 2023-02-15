a7eda07684584530e50fd93a617369deb6ce36b0
Oumaima-el <elkhattabioumaima97@gmail.com>
2023-02-15 21:35:43 +0100

    inventory-service

---
+++ inventory-service/src/main/java/ma/enset/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,35 @@
+package ma.enset.inventoryservice;
+
+import ma.enset.inventoryservice.entities.Product;
+import ma.enset.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import java.util.List;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository,
+                            RepositoryRestConfiguration restConfiguration){
+        return args -> {
+            restConfiguration.exposeIdsFor(Product.class);
+            productRepository.saveAll(
+                    List.of(
+                            Product.builder().designation("Ordinateur").price(788).quantity(12).build(),
+                            Product.builder().designation("Imprimante").price(88).quantity(129).build(),
+                            Product.builder().designation("Ordinateur").price(1288).quantity(112).build()
+                    )
+            );
+        };
+    }
+
+}
---
+++ inventory-service/src/main/java/ma/enset/inventoryservice/entities/Product.java
@@ -0,0 +1,18 @@
+package ma.enset.inventoryservice.entities;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String designation;
+    private double price;
+    private int quantity;
+}
